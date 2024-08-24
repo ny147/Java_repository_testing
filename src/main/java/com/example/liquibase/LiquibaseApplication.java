494314d9d1bcd0a065d23bf69cc0017a4c7f055d
ny147 <nearyou1474@gmail.com>
2024-08-24 22:33:18 +0700

    update bug

--- src/main/java/com/example/liquibase/LiquibaseApplication.java
+++ src/main/java/com/example/liquibase/LiquibaseApplication.java
@@ -2,7 +2,7 @@ package com.example.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//dasd
 @SpringBootApplication
 public class LiquibaseApplication {
 
